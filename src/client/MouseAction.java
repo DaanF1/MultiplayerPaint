890ca6e1687252770ea0d0eabd3da2b2620be3b1
tiemenbr <tiemenbr@hotmail.nl>
2024-05-23 10:49:32 +0200

    added Action classes for PaintClient
    PaintClient should preferably only be javafx code

---
+++ src/client/MouseAction.java
@@ -0,0 +1,35 @@
+package client;
+
+import canvas.CanvasObject;
+import canvas.LineSegment;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class MouseAction {
+
+    private Point2D lastMousePosition;
+    private Point2D currentMousePosition;
+
+    public MouseAction() {
+    }
+
+    public void mousePressed(MouseEvent e) {
+        lastMousePosition = new Point2D.Double(e.getX(), e.getY());
+    }
+
+    public void mouseReleased(MouseEvent e, Socket clientSocket, ArrayList<CanvasObject> canvasObjects) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+        oos.writeObject(canvasObjects);
+    }
+
+    public void mouseDragged(MouseEvent e, ArrayList<CanvasObject> canvasObjects) {
+        currentMousePosition = new Point2D.Double(e.getX(), e.getY());
+        canvasObjects.add(new LineSegment(lastMousePosition, currentMousePosition));
+        lastMousePosition = currentMousePosition;
+    }
+}
